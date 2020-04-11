5fb21b55b6701a0aff51e32e3864dac3a3226f3b
trqminh <1612374@student.hcmus.edu.vn>
2020-04-11 22:32:47 +0700

    on modifying

--- algorithms/dqn_stack_action/my_dqn_stack_action.py
+++ algorithms/dqn_stack_action/my_dqn_stack_action.py
@@ -13,10 +13,14 @@ import os
 import numpy as np
 import random
 from ast import literal_eval
+from itertools import count
+from collections import namedtuple
 
 torch.manual_seed(1434)
 np.random.seed(1434)
 
+Transition = namedtuple('Transition',
+                        ('state', 'act', 'reward', 'next_state', 'done_mask'))
 
 class ReplayMemory(object):
     def __init__(self, capacity):
@@ -36,6 +40,34 @@ class ReplayMemory(object):
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
+
+
+def optimize_network(Q_net, Q_target_net, batch):
+    # SAMPLE MINIBATCH AND OPTIMIZE
+    batch_done_mask = torch.cat(batch.done_mask).to(device=device, dtype=torch.float)
+    batch_reward = torch.cat(batch.reward).to(device=device, dtype=torch.float)
+    print(batch_reward.shape, batch_done_mask.shape)
+
+
+    b_next_states = np.stack(b_transitions[:,3])
+    b_next_states = torch.from_numpy(b_next_states).to(device=device, dtype=torch.float)
+
+    y = b_reward + b_reward_mask * (Q_target_net(b_next_states).max(1)[0]) * gamma
+
+    # compute predict value
+    b_pre_states = np.stack(b_transitions[:,0])
+    b_pre_states = torch.from_numpy(b_pre_states).to(device=device, dtype=torch.float)
+
+    b_acts = np.stack(b_transitions[:,1])
+    b_acts = torch.from_numpy(b_acts).to(device=device, dtype=torch.long).unsqueeze(1)
+
+    pred_Q = torch.gather(Q_net(b_pre_states), 1, b_acts)
+
+    # optimize mse
+    optimizer.zero_grad()
+    loss = torch.mean((y - pred_Q)**2)
+    loss.backward()
+    optimizer.step()
 
 
 def train(configs):
@@ -65,7 +97,6 @@ def train(configs):
     D = ReplayMemory(replay_mem_size)
     Q_net.train()
     Q_target_net.eval()
-    step = 0
     ep_rets = []
 
     # TRAINING
@@ -73,55 +104,36 @@ def train(configs):
         loss = 1000000000.0
         ep_ret = 0.
         done = False
-        obs = env.reset()
+        obs = torch.from_numpy(env.reset()).to(device=device, dtype=torch.float).unsqueeze(0)
 
-        while True:
+        for step in count():
             # SELECT ACTION WITH EPSILON GREEDY
             epsilon = random.uniform(0,1)
             if epsilon < ep_thresh:
                 act = random.randint(0, n_acts - 1)
             else:
-                obs_t = torch.from_numpy(obs).to(device=device, dtype=torch.float)
-                act = torch.argmax(Q_net(obs_t)).item()
+                with torch.no_grad():
+                    act = torch.argmax(Q_net(obs)).item()
 
             # EXCUTE ACTION AND STORE TRANSITION
-            pre_obs = copy.deepcopy(obs)
-            obs, reward, done, info = env.step(act)
+            next_obs, reward, done, _ = env.step(act)
             ep_ret += reward
-            D.push([pre_obs, act, reward, obs, done])
 
-            # SAMPLE MINIBATCH AND OPTIMIZE
+            next_obs = torch.from_numpy(next_obs).to(device=device,dtype=torch.float).unsqueeze(0)
+            reward = torch.tensor([float(reward)], device=device)
+            done_mask = torch.tensor([1 - int(done)], device=device)
+
+            D.push([obs, act, reward, next_obs, done_mask])
+            obs = next_obs
+
+            # OPTIMIZATION
             if len(D) < batch_size:
                 continue
 
-            # compute target value
-            b_transitions = np.array(D.sample(batch_size))
+            transitions = D.sample(batch_size)
+            batch = Transition(*zip(*transitions))
 
-            b_reward_mask = np.bitwise_xor(b_transitions[:,-1].astype(int), np.ones(batch_size, dtype=int))
-            b_reward_mask = torch.from_numpy(b_reward_mask).to(device=device, dtype=torch.float)
-
-            b_reward = np.stack(b_transitions[:,2])
-            b_reward = torch.from_numpy(b_reward).to(device=device, dtype=torch.float)
-
-            b_next_states = np.stack(b_transitions[:,3])
-            b_next_states = torch.from_numpy(b_next_states).to(device=device, dtype=torch.float)
-
-            y = b_reward + b_reward_mask * (Q_target_net(b_next_states).max(1)[0]) * gamma
-
-            # compute predict value
-            b_pre_states = np.stack(b_transitions[:,0])
-            b_pre_states = torch.from_numpy(b_pre_states).to(device=device, dtype=torch.float)
-
-            b_acts = np.stack(b_transitions[:,1])
-            b_acts = torch.from_numpy(b_acts).to(device=device, dtype=torch.long).unsqueeze(1)
-
-            pred_Q = torch.gather(Q_net(b_pre_states), 1, b_acts)
-
-            # optimize mse
-            optimizer.zero_grad()
-            loss = torch.mean((y - pred_Q)**2)
-            loss.backward()
-            optimizer.step()
+            optimize_network(Q_net, Q_target_net, batch)
 
             # RESET TARGET Q NETWORK
             step += 1
