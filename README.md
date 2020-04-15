de31d5a5e15408a5b34fb1466080c834eba5b7af
trqminh <1612374@student.hcmus.edu.vn>
2020-04-15 10:28:07 +0700

    exp modification: 4th - finish drawing

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 - Batch size: 5000
 - Model: MLP 1 hidden layer (hidden size = 32)
 - Optimizer: Adam (lr = 1e-2)
-- Algorithms: Simple PG, Vanilla PG (Simple PG + baseline)
-![Mean episode return through each epoch](./foo.png)
+- Algorithms: Simple PG, Vanilla PG (Simple PG, actor critic network)
+![Mean episode return through each epoch](./experiments/exp_1/ret.png)
+*Mean episode return through each epoch*
+![Mean episode length through each epoch](./experiments/exp_1/len.png)
+*Mean episode length through each epoch*
 
 Coming late: DQN, TRPO, PPO, DDPO
--- draw.py
+++ draw.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -13,24 +14,33 @@ def parse_args():
 def main():
     args = parse_args()
     exp_name = args.exp_name
+    exp_path = './experiments/' + exp_name + '/'
 
-    exp = os.listdir('./experiments/' + exp_name + '/')
-    subs = 'ret'
-    ret_info = [i for i in info if subs in i]
+    exp_result_files = os.listdir(exp_path)
+    for item in ['ret', 'len']:
+        file_names = [i for i in exp_result_files if item in i]
+        epochs = [i for i in range(1, 1001)]
 
-    epochs = [i for i in range(1, 1001)]
-    labels = ['vanilla_pg', 'simple_pg']
+        for file_name in file_names:
+            rets = []
+            ext = os.path.splitext(file_name)[1]
+            if ext != '.csv':
+                continue
 
-    for i, ret_file in enumerate(ret_info):
-        rets = []
-        with open('./info/' + ret_file, 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                rets.append(float(row[0]))
+            with open(exp_path + file_name, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    rets.append(float(row[0]))
 
-        plt.plot(epochs, rets, label=labels[i])
+            label = os.path.splitext(file_name)[0]
+            plt.plot(epochs, rets, label=label)
 
-    plt.ylabel('mean episode return')
-    plt.xlabel('Epoch')
-    plt.legend(loc='lower right')
-    plt.savefig('foo.png')
+        plt.ylabel('mean episode' + item)
+        plt.xlabel('Epoch')
+        plt.legend(loc='lower right')
+        plt.savefig(exp_path + item + '.png')
+        plt.clf()
+
+
+if __name__ == '__main__':
+    main()
