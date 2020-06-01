a18bdbaf81782f9388a9d259ae92492467aa9a7e
trqminh <1612374@student.hcmus.edu.vn>
2020-06-01 10:45:03 +0700

    done plan for ddpg

--- algorithms/my_ddpg.py
+++ algorithms/my_ddpg.py
@@ -52,6 +52,8 @@ class DDPG(object):
         self.n_episodes = configs['n_episodes']
         self.memory_size = configs['memory_size']
         self.gamma = configs['gamma']
+        self.target_update_step = configs['target_update_step']
+        self.global_step = 0
 
         # save the "best" policy
         self.best_mean_episode_ret = -1e6
@@ -67,22 +69,54 @@ class DDPG(object):
                         output_activation=output_activation).to(self.device)
         self.target_actor = mlp(sizes = [self.obs_dim] + self.hidden_sizes + [self.n_acts], 
                         output_activation=output_activation).to(self.device)
+        self.target_actor.load_state_dict(self.actor.state_dict())
 
         self.critic = Critic(self.obs_dim, self.n_acts)
         self.target_critic = Critic(self.obs_dim, self.n_acts)
-        # TODO: get the target copy the original
+        self.target_critic.load_state_dict(self.critic.state_dict())
+
         self.replay_memory = self.ReplayMemory(self.memory_size)
+        self.optimizer = optim.Adam(
+                list(self.actor.parameters()) + list(self.critic.parameters()),
+                lr=self.lr
+            )
+
+    def optimize(self):
+        # SAMPLE FROM REPLAY MEMORY
+        batch = self.Transition(*zip(*transitions))
+
+        batch_done_mask = torch.cat(batch.done_mask)
+        batch_reward = torch.cat(batch.reward)
+        batch_state = torch.cat(batch.state)
+        batch_act = torch.cat(batch.act)
+        batch_next_state = torch.cat(batch.next_state)
+
+        target = batch_reward + self.gamma * batch_done_mask * \
+                (self.target_critic(batch_next_state, self.target_actor(batch_next_state)).detach())
+        predict_target = self.critic(batch_state, self.actor(batch_state))
+        loss = torch.mean((target - predict_target)**2)
+
+        # OPTIMIZE ACTOR AND CRITIC
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # RESET TARGET NETWORK
+        if self.global_step > 0 and self.global_step % self.target_update_step == 0:
+            self.target_actor.load_state_dict(self.actor.state_dict())
+            self.target_critic.load_state_dict(self.critic.state_dict())
+            # TODO: how to update as ddpg type
+
+
 
     def train(self):
         # TRAINING
         for ep in range(self.n_episodes):
-            loss = None
-            ep_ret = 0.
-            done = False
+            ep_ret, done = 0., False
             obs = torch.from_numpy(self.env.reset()).to(device=device, dtype=torch.float).unsqueeze(0)
 
             for t in count():
-                global_step += 1
+                self.global_step += 1
                 # SELECT ACTION
                 with torch.no_grad():
                     act = self.actor(obs) # TODO: + epsilone ~ normal(0,1)
@@ -99,47 +133,18 @@ class DDPG(object):
                 obs = next_obs
 
                 # OPTIMIZATION
-                if len(D) < batch_size: # TODO: and time to update
-                    continue
-
-                # TODO: split optimize into a method
-                transitions = self.replay_memory.sample(batch_size)
-                batch = self.Transition(*zip(*transitions))
-
-                batch_done_mask = torch.cat(batch.done_mask)
-                batch_reward = torch.cat(batch.reward)
-                batch_state = torch.cat(batch.state)
-                batch_act = torch.cat(batch.act)
-                batch_next_state = torch.cat(batch.next_state)
-
-                target = batch_reward + self.gamma * batch_done_mask * \
-                        (self.target_critic(batch_next_state, self.target_actor(batch_next_state)).detach())
-                predict_target = self.critic(batch_state, self.actor(batch_state))
-
-                loss = torch.mean((target - predict_target)**2)
-
-                # TODO: optimizer and target update
-                '''
-                optimizer.zero_grad()
-                loss.backward()
-                for param in Q_net.parameters():
-                    param.grad.data.clamp_(-1, 1)
-                optimizer.step()
-
-                # RESET TARGET Q NETWORK
-                if global_step % target_update_step == 0:
-                    Q_target_net.load_state_dict(Q_net.state_dict())
+                if len(D) >= batch_size: # TODO: and time to update
+                    self.optimize()
 
                 if done:
                     ep_rets.append(ep_ret)
                     ep_lens.append(t + 1)
                     break
-                '''
 
             # TODO: what is need to log
             if ep % 20 == 0:
-                print('Episode {}, loss: {:.2f}, mean episode length: {:.2f}, mean episode return: {:.2f}'.format(
-                    ep, loss, np.mean(ep_lens), np.mean(ep_rets)))
+                print('Episode {}, mean episode length: {:.2f}, mean episode return: {:.2f}'.format(
+                    ep, np.mean(ep_lens), np.mean(ep_rets)))
                 ep_rets = []
                 ep_lens = []
 
